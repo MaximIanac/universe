2115771249c81246f05429a7fe91d476aa1d3a78
@maximianac123 <maxim_ianac1@list.ru>
2023-06-27 11:23:22 +0300

    edit

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/universe'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
